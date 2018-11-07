8b0614f404954375c0b723fb6cb32d754f7d166b
wise700 <hrushikesh1988@gmail.com>
2018-11-08 00:32:14 +0530

    Completed Loan Prediction

--- Projects/Loan Prediction/LoanPrediction.py
+++ Projects/Loan Prediction/LoanPrediction.py
@@ -8,6 +8,8 @@ import pandas as pd
 import matplotlib
 import seaborn as sns
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import csv
 
 from sklearn.linear_model import LogisticRegression  
 
@@ -38,43 +40,74 @@ Y_Train=train['Loan_Status'];
 train.drop('Loan_Status',axis=1,inplace=True);
 #X_Train=pd.get_dummies(train);
 
+all_data=train.append(test,ignore_index=True);
 
+############################Missing Values############################
 #Handling nan values in Dataset
-print(train.isnull().sum());
+print(all_data.isnull().sum());
 
 #Gender replace nan by male
-print(train['Married'].value_counts());
-train['Gender'].fillna('Male',inplace=True);
-print(train['Gender'].isnull().sum());
+print(all_data['Married'].value_counts());
+all_data['Gender'].fillna('Male',inplace=True);
+print(all_data['Gender'].isnull().sum());
 
 #Married replaced nan by Yes
 
-train['Married'].fillna('Yes',inplace=True);
+all_data['Married'].fillna('Yes',inplace=True);
 
 #Dependents replace nan by 0
-train['Dependents'].fillna('0',inplace=True);
-print(train['Dependents'].value_counts());
+all_data['Dependents'].fillna('0',inplace=True);
+print(all_data['Dependents'].value_counts());
       
-
-print(train.isnull().sum());
-
 #Self_Employed replace nan by No
-print(train['Self_Employed'].value_counts());
-train['Self_Employed'].fillna('No',inplace=True);
+print(all_data['Self_Employed'].value_counts());
+all_data['Self_Employed'].fillna('No',inplace=True);
 
 #Loan Amount replace nan by Mean of Loan aminuts
-train['LoanAmount'].fillna(train['LoanAmount'].mean(),inplace=True);
+all_data['LoanAmount'].fillna(all_data['LoanAmount'].mean(),inplace=True);
 
 #Loan Term replace nan by Mean of Loan Term
-train['Loan_Amount_Term'].fillna(train['Loan_Amount_Term'].mean(),inplace=True);
-print(train['Credit_History'].value_counts());
+all_data['Loan_Amount_Term'].fillna(all_data['Loan_Amount_Term'].mean(),inplace=True);
+print(all_data['Credit_History'].value_counts());
       
 #Credit_History replace nan by 1
-train['Credit_History'].fillna(1,inplace=True);
+all_data['Credit_History'].fillna(1,inplace=True);
 
-#
+print(all_data.isnull().sum());
+
+############################Missing Values############################
+
+
+############################Categorical Variables######################
+
+all_data_with_dummies=pd.get_dummies(all_data);
+
+##with Label encoder you can do only one column at a time, then use ohe to get same as pd.e=get_dummies
+LE =LabelEncoder();
+#LE_data_ohe = LE.fit_transform(all_data['Married']);
+
+#encode the output also
+Y_Train=LE.fit_transform(Y_Train);
+############################Categorical Variables######################
+
+#Split train test
+
+X_Train=all_data_with_dummies[:train.shape[0]];
+Y_Test=all_data_with_dummies[train.shape[0]:];
+
+
 ##Model
-#LG=LogisticRegression();
-#
-#LG.fit(X_Train,Y_Train);
+LG=LogisticRegression();
+
+LG.fit(X_Train,Y_Train);
+
+pred=LG.predict(Y_Test);
+
+prede=list(LE.inverse_transform(pred));
+
+with open('pred.csv', 'w',newline='') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows(prede)
+    
+writeFile.close()
 
